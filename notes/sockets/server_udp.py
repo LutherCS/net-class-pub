35b76a304ca63c506c64922e1127b06721ce22b8
Roman Yasinovskyy <yasinovskyy@gmail.com>
2022-09-30 10:13:26 -0500

    Add error handling loop

--- notes/sockets/server_udp.py
+++ notes/sockets/server_udp.py
@@ -1,14 +1,24 @@
 import socket
 from socket import SOCK_DGRAM, AF_INET
+from time import sleep
+from random import randint
 
 host = "127.0.0.2"
 port = 4300
 
+
 def main():
     print("Server here")
     sock = socket.socket(AF_INET, SOCK_DGRAM)
-    sock.bind((host, port))
-    
+
+    while True:
+        try:
+            sock.bind((host, port))
+            break
+        except OSError as os_err:
+            print(os_err)
+            sleep(randint(1, 5))
+
     while True:
         msg, client = sock.recvfrom(2048)
         msg = msg.decode()
