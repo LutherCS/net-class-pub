1f5dde8c70633bf60db050d00450481839c1d254
Roman Yasinovskyy <yasinovskyy@gmail.com>
2022-12-01 11:45:56 -0600

    Update routing tests

--- tests/projects/routing/test_router.py
+++ tests/projects/routing/test_router.py
@@ -1,42 +1,64 @@
 #!/usr/bin/python3
 """
-Testing Router
+Router testing using UDP sockets
+
+@author: Roman Yasinovskyy
+@version: 2022.12
 """
 
 
+import importlib
+import pathlib
+import sys
+
 import mock
 import pytest
-from src.projects.routing.udp_router import *
 
-ROUTING_TABLE = {
-    "127.0.0.2": [1, "127.0.0.2"],
-    "127.0.0.3": [3, "127.0.0.3"],
-    "127.0.0.4": [7, "127.0.0.4"],
-}
+try:
+    importlib.util.find_spec(".".join(pathlib.Path(__file__).parts[-3:-1]), "src")
+except ModuleNotFoundError:
+    sys.path.append(f"{pathlib.Path(__file__).parents[3]}/")
+finally:
+    from src.projects.routing.udp_router import (
+        format_hello,
+        format_update,
+        parse_hello,
+        parse_update,
+        read_config_file,
+    )
+
+
+@pytest.fixture(name="routing_table")
+def fixture_routing_table():
+    return {
+        "127.0.0.2": [1, "127.0.0.2"],
+        "127.0.0.3": [3, "127.0.0.3"],
+        "127.0.0.4": [7, "127.0.0.4"],
+    }
 
 
 @mock.patch("src.projects.routing.udp_router.THIS_HOST", "127.0.0.1")
-def test_read_config_file():
+def test_read_config_file(routing_table):
     """Read config file"""
     s, d = read_config_file("data/projects/routing/network_simple.txt")
-    assert s == set(ROUTING_TABLE.keys())
-    assert d == ROUTING_TABLE
+    assert s == set(routing_table.keys())
+    assert d == routing_table
 
 
 def test_read_config_file_error():
     """Read config file"""
-    with pytest.raises(FileNotFoundError) as fnfe:
+    with pytest.raises(FileNotFoundError) as fnf_err:
         read_config_file("data/projects/routing/wrong_file.txt")
     assert (
-        str(fnfe.value)
+        str(fnf_err.value)
         == "Could not find the specified configuration file data/projects/routing/wrong_file.txt"
     )
 
 
-def test_format_update():
+def test_format_update(routing_table):
     """Format UPDATE message"""
     assert (
-        format_update(ROUTING_TABLE)
+        format_update(routing_table)
         == b"\x00\x7f\x00\x00\x02\x01\x7f\x00\x00\x03\x03\x7f\x00\x00\x04\x07"
     )
 
@@ -49,9 +71,9 @@ def test_format_update():
     ],
 )
 @mock.patch("src.projects.routing.udp_router.THIS_HOST", "127.0.0.1")
-def test_parse_update(message, addr, status):
+def test_parse_update(routing_table, message, addr, status):
     """Parse UPDATE message"""
-    assert parse_update(message, addr, ROUTING_TABLE) == status
+    assert parse_update(message, addr, routing_table) == status
 
 
 @pytest.mark.parametrize(
@@ -90,10 +112,10 @@ def test_format_hello(msg_txt, src, dst, msg_bytes):
     ],
 )
 @mock.patch("src.projects.routing.udp_router.THIS_HOST", "127.0.0.1")
-def test_parse_hello(message, result):
+def test_parse_hello(routing_table, message, result):
     """Parse HELLO message"""
-    assert parse_hello(message, ROUTING_TABLE) == result
+    assert parse_hello(message, routing_table) == result
 
 
 if __name__ == "__main__":
-    pytest.main(["test_router.py"])
+    pytest.main(["-v", __file__])
